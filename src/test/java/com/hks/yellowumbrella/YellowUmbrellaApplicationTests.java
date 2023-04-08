eea65ecd36fe5db0fb4e359edb02138272bc916d
hankyungsu <hks713@naver.com>
2023-04-08 10:54:23 +0900

    first commit

---
+++ src/test/java/com/hks/yellowumbrella/YellowUmbrellaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hks.yellowumbrella;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YellowUmbrellaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
